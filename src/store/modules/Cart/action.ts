21c78deeb0d608dd89c3eed780de08f125d39547
FlankSilva <flank.silva.0@gmail.com>
2020-11-03 11:09:46 -0300

    Readux

---
+++ src/store/modules/Cart/action.ts
@@ -0,0 +1,28 @@
+import { ActionTypes, IProduct } from './types';
+
+export const addProductToCartRequest = (product: IProduct) => {
+  return {
+    type: ActionTypes.addProductToCartReqest,
+    payload: {
+      product,
+    },
+  };
+};
+
+export const addProductToCartSuccess = (product: IProduct) => {
+  return {
+    type: ActionTypes.addProductToCartSuccess,
+    payload: {
+      product,
+    },
+  };
+};
+
+export const addProductToCartFailure = (productId: number) => {
+  return {
+    type: ActionTypes.addProductToCartFailure,
+    payload: {
+      productId,
+    },
+  };
+};
